042c9b61f41cad0c4610f5c9ecd71c06d6a145a6
Aladine Belhaj <alabelhaj2002@gmail.com>
2023-07-26 17:47:13 +0100

    Dashboard design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -14,7 +14,7 @@ import { SidebarComponent } from './dashboard/sidebar/sidebar.component'
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent, AddticketComponent, SidebarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule ,MatDialogModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule ,MatDialogModule,MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
