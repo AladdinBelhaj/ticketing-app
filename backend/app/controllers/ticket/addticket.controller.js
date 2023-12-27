4fb2e10b2a0191dd37999bb9f82a499d5ec3ab50
Aladine Belhaj <alabelhaj2002@gmail.com>
2023-12-27 14:40:13 +0100

    fix backend altresponsable not updating

--- backend/app/controllers/ticket/addticket.controller.js
+++ backend/app/controllers/ticket/addticket.controller.js
@@ -20,11 +20,11 @@ const setupAddTicketRoutes = (app) => {
 
 
   app.post("/ticket", upload.fields([{ name: "fichier" }]), (req, res) => {
-    const { projet, objet, emitteur, description, etat, responsable, descriptionSolution } = req.body;
+    const { projet, objet, emitteur, description, etat, responsable,altResponsable, descriptionSolution } = req.body;
     const fichier = req.files && req.files["fichier"] ? req.files["fichier"][0].filename : null;
   
-    const insertQuery = `INSERT INTO ticket (projet, objet, emitteur, description, fichier, etat, responsable, descriptionSolution) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-    const values = [projet, objet, emitteur, description, fichier, etat, responsable, descriptionSolution];
+    const insertQuery = `INSERT INTO ticket (projet, objet, emitteur, description, fichier, etat, responsable, altResponsable, descriptionSolution) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [projet, objet, emitteur, description, fichier, etat, responsable,altResponsable, descriptionSolution];
   
     connexion.query(insertQuery, values, (err, results) => {
       if (err) {
@@ -118,11 +118,12 @@ const setupAddTicketRoutes = (app) => {
         description,
         etat,
         responsable,
+        altResponsable,
         descriptionSolution,
       } = req.body;
 
       let updateQuery =
-        "UPDATE ticket SET projet = ?, objet = ?, emitteur = ?, description = ?, etat = ?, responsable = ?, descriptionSolution = ?";
+        "UPDATE ticket SET projet = ?, objet = ?, emitteur = ?, description = ?, etat = ?, responsable = ?, altResponsable = ?, descriptionSolution = ?";
 
       const updateValues = [
         projet,
@@ -132,6 +133,8 @@ const setupAddTicketRoutes = (app) => {
         etat,
         responsable,
         descriptionSolution,
+        altResponsable
+
       ];
 
       // Check if "fichierSolution" has been uploaded
